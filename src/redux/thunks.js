7f24e1e6a389bc9734d413cfc6b15fff47e4cb48
Artem <a.g.sulimko@gmail.com>
2024-02-15 19:13:40 +0100

    updata

--- src/redux/thunks.js
+++ src/redux/thunks.js
@@ -1,20 +1,3 @@
-// import { createAsyncThunk } from '@reduxjs/toolkit';
-
-// import { getAdverts, getFavorites } from 'api/api';
-// export const fetchAdverts = createAsyncThunk(
-//   'adverts/fetchAdverts',
-//   async (page, thunkAPI) => {
-//     try {
-//       const response = await getAdverts(page);
-
-//       return response;
-//     } catch (e) {
-//       console.log(e.message);
-//       return thunkAPI.rejectWithValue(e.message);
-//     }
-//   }
-// );
-
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { getAdverts, getFavorites } from 'api/api';
