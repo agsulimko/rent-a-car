c9f1b4585dc157f431d9e5a887dabfbd9e55c681
Artem <a.g.sulimko@gmail.com>
2024-03-14 23:11:20 +0100

    update HomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,26 @@
 import { Container } from "styles/Container/Container";
 
-import { H1, H2, MainHome } from "./HomeStyled";
+import { H1, H2, MainHome, Img1, Img2 } from "./HomeStyled";
 const Home = () => {
   return (
     <Container>
       <MainHome>
         <section>
+          <Img1
+            src="https://ftp.goit.study/img/cars-test-task/audi_a5.jpeg"
+            alt="audi_a5"
+            width={370}
+          />
           <H1>Welcome to Rent a Car!</H1>
           <H2>
             The Rent a Car app is an easy to use app that created for a company
             that provides car rental services in Ukraine. Try today!
           </H2>
+          <Img2
+            src="https://ftp.goit.study/img/cars-test-task/lamborghini_murcielago.jpeg"
+            alt="lamborghini"
+            width={400}
+          />
         </section>
       </MainHome>
     </Container>
--- src/pages/HomeStyled.js
+++ src/pages/HomeStyled.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 const H1 = styled.h1`
-  padding-top: 350px;
+  font-size: 56px;
+  padding-top: 390px;
   margin-bottom: 40px;
   text-align: center;
   color: white;
@@ -15,7 +16,23 @@ const H2 = styled.h2`
   margin: 0 auto;
 `;
 
+const Img1 = styled.img`
+  position: absolute;
+  left: 100px;
+  top: 100px;
+  border-radius: 20px;
+  /* transform: rotate(-20deg); */
+`;
+const Img2 = styled.img`
+  position: absolute;
+  right: 100px;
+  bottom: 100px;
+  border-radius: 20px;
+  /* transform: rotate(-20deg); */
+`;
+
 const MainHome = styled.main`
+  position: relative;
   background-color: dimgray;
   height: 1000px;
   background-repeat: no-repeat;
@@ -29,4 +46,4 @@ const MainHome = styled.main`
     url('https://ftp.goit.study/img/cars-test-task/pontiac_firebird.jpeg');
 `;
 
-export { H1, H2, MainHome };
+export { H1, H2, MainHome, Img1, Img2 };
