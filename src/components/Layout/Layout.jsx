9323475248bdcaaaa9035a60e124a9f6fef8d8df
Artem <a.g.sulimko@gmail.com>
2024-05-01 13:37:37 +0200

    add Icon

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import styled from "styled-components";
-import { NavLink, Outlet, useLocation } from "react-router-dom";
+import { Link, NavLink, Outlet, useLocation } from "react-router-dom";
 import { UlListLayout } from "./Layout.styled";
 import { Container as BaseContainer } from "styles/Container/Container";
 import { Header as BaseHeader } from "./Layout.styled";
 
+import autoIcon from "../../image/sport-car.png";
+const LogoLink = styled(Link)`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  font-size: 19px;
+  font-weight: 600;
+
+  gap: 10px;
+  top: 15px;
+  left: 25px;
+`;
 const StyledLink = styled(NavLink)`
   padding: 12px 30px;
   border-radius: 12px;
@@ -43,6 +55,7 @@ const Container = styled(BaseContainer)`
 
 const Header = styled(BaseHeader)`
   &.active-catalog {
+    position: relative;
     border-bottom: 3px solid gray;
     box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
     background-color: #bfd1f7;
@@ -66,7 +79,14 @@ const Layout = () => {
       <Header
         className={isActiveCatalog || isActiveFavorites ? "active-catalog" : ""}
       >
+        <LogoLink to="/">
+          <img src={autoIcon} alt="auto" width={40} />
+          <p>Rent-a-car</p>
+        </LogoLink>
         <UlListLayout>
+          <Link to="/"></Link>
+          <UlListLayout></UlListLayout>
+
           <li>
             <StyledHomeLink
               to="/"
